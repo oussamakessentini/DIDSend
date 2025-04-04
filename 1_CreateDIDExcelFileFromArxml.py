71f5ac57211010a45ed7e04e63fe3f0ab4281710
Ahmed TEIR <ahmed.teir.ext@e2-cad.com>
2025-04-04 15:06:32 +0200

    Minor changes

--- 1_CreateDIDExcelFileFromArxml.py
+++ 1_CreateDIDExcelFileFromArxml.py
@@ -39,42 +39,28 @@ def extractDataFromArxml(file_path):
                         # Check specific DID => DcmDspData
                         if did_dataInfo.text == did_dataRef:
                             did_size = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspDataSize")
-                            did_ReadFunction = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspDataReadFnc")
-                            did_WriteFunction = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspDataWriteFnc")
-                            size = ""
-                            Read_fct = ""
-                            Write_fct = ""
+                            did_read_fct = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspDataReadFnc")
+                            did_wead_fct = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspDataWriteFnc")
+
                             if did_size is not None:
                                 # Add +7 to rounds up and get the right size (byte numbers)
                                 # Note : // operator is the floor division operator => rounds down to the nearest whole number
-                                size = (int(did_size) + 7) // 8
-                            if did_ReadFunction is not None:
-                                Read_fct = did_ReadFunction
-                            if did_WriteFunction is not None:
-                                Write_fct = did_WriteFunction
-                            did_data.append({"DID": did_id, "Size" : size, "Read": Read_fct, "Write": Write_fct})
+                                did_size = (int(did_size) + 7) // 8
+
+                            did_data.append({"DID": did_id, "Size" : did_size, "Read": did_read_fct, "Write": did_wead_fct})
                             break
             
             # Retreive RC
             if def_Val is not None and def_Val.text.endswith("Dcm/DcmConfigSet/DcmDsp/DcmDspRoutine"):
-                rc_start = find_recursive_Value(data, "DEFINITION-REF", "DcmDspStartRoutineFnc")
-                rc_stop = find_recursive_Value(data, "DEFINITION-REF", "DcmDspStopRoutineFnc")
-                rc_result = find_recursive_Value(data, "DEFINITION-REF", "DcmDspRequestResultsRoutineFnc")
+                rc_start_fct = find_recursive_Value(data, "DEFINITION-REF", "DcmDspStartRoutineFnc")
+                rc_stop_fct = find_recursive_Value(data, "DEFINITION-REF", "DcmDspStopRoutineFnc")
+                rc_result_fct = find_recursive_Value(data, "DEFINITION-REF", "DcmDspRequestResultsRoutineFnc")
                 rc_id = find_recursive_Value(data, "DEFINITION-REF", "DcmDspRoutineIdentifier")
                 # Convert and clean RC ID format
                 rc_id = str(hex(int(rc_id))).upper()[2:].zfill(4)
                 rc_info = find_recursive_Value(data, "DEFINITION-REF", "DcmDspRoutineInfoRef")
                 # Get RC Info definition from DcmDspRoutineInfoRef
                 rc_info = rc_info.split('/')[-1]
-                start_fct = ""
-                stop_fct = ""
-                result_fct = ""
-                if rc_start is not None:
-                    start_fct = rc_start
-                if rc_stop is not None:
-                    stop_fct = rc_stop
-                if rc_result is not None:
-                    result_fct = rc_result
                 
                 # Retreive RC information
                 for data_s in root.iter("ECUC-CONTAINER-VALUE"):
@@ -82,7 +68,7 @@ def extractDataFromArxml(file_path):
                     
                     if def_Val is not None and def_Val.text.endswith("Dcm/DcmConfigSet/DcmDsp/DcmDspRoutineInfo"):
                         rc_dataInfo = find_recursive(data_s, "SHORT-NAME")
-                        # Check specific RC => DcmDspData
+                        # Check specific RC => DcmDspRoutineInfo
                         if rc_dataInfo.text == rc_info:
 
                             rc_start_dataIn_size = find_recursive_Value(data_s, "DEFINITION-REF", "DcmDspStartRoutineInSignal/DcmDspRoutineSignalLength")
@@ -109,9 +95,9 @@ def extractDataFromArxml(file_path):
                             if rc_result_dataOut_size is not None:
                                 rc_result_dataOut_size = (int(rc_result_dataOut_size) + 7) // 8
                             
-                            rc_data.append({"RC ID": rc_id, "Start RC": start_fct, "Start DataIn": rc_start_dataIn_size, "Start DataOut": rc_start_dataOut_size, \
-                                                            "Stop RC": stop_fct, "Stop DataIn": rc_stop_dataIn_size, "Stop DataOut": rc_stop_dataOut_size, \
-                                                            "Result RC": result_fct, "Result DataIn": rc_result_dataIn_size, "Result DataOut": rc_result_dataOut_size})
+                            rc_data.append({"RC ID": rc_id, "Start RC": rc_start_fct, "Start DataIn": rc_start_dataIn_size, "Start DataOut": rc_start_dataOut_size, \
+                                                            "Stop RC": rc_stop_fct, "Stop DataIn": rc_stop_dataIn_size, "Stop DataOut": rc_stop_dataOut_size, \
+                                                            "Result RC": rc_result_fct, "Result DataIn": rc_result_dataIn_size, "Result DataOut": rc_result_dataOut_size})
                             break
     else:
         print(f"extractDataFromArxml: {file_path} is not present")
